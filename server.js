b288b5c248c06bba44a478e5095ca318fa7ae43e
shaikhalfaj142 <shaikhalfaj142@gmail.com>
2026-07-10 16:57:35 +0530

    my backend new

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ require("dotenv").config();
 
 const express = require("express");
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 const app = express();
 
 connectDB();
+
+app.use(cors());
+
 
 app.use(express.json());
 
@@ -20,8 +24,20 @@ app.get("/", (req, res) => {
   res.send("API Running...");
 });
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5174",
+      "https://alfajshaikh.github.io",
+    ],
+    credentials: true,
+  })
+);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+
+
 });
